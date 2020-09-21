5be4324ae09bc3466a09203395d81d587f49c7d7
AK <akif448@gmail.com>
2020-09-21 19:27:53 +1000

    assets folder created, css updated

--- script.js
+++ script.js
@@ -1,83 +1,84 @@
-$(document).ready (function(){
-  
-    $("#run-search").on("click",function(e){
-      e.preventDefault();
+$(document).ready(function () {
 
-      //get the selected news source
-      var selection =$('#dropdownNews :selected').val();
+  $("#run-search").on("click", function (e) {
+    e.preventDefault();
 
-      //clear the news source images 
-      $("#newsSource").empty();
+    //get the selected news source
+    var selection = $('#dropdownNews :selected').val();
 
-      //get the image for the selected news source
-      var img = document.createElement("img");
-      if (selection==="Gardian"){
-        img.src = " The_Guardian_logo.png";
-      } else {
-        img.src = "The_New_York_Times_logo.png";
-      }
-      var src = document.getElementById("newsSource");
-      src.appendChild(img);
+    //clear the news source images 
+    $("#newsSource").empty();
 
-      // clear the previous contents displayed
-      clear();
-      
-      // get the result 
-      if (selection ==="Gardian") {
-      
+    //get the image for the selected news source
+    var img = document.createElement("img");
+    if (selection === "Gardian") {
+      img.src = " assets/The_Guardian_logo.png";
+    } else {
+      img.src = " assets/The_New_York_Times_logo.png";
+    }
+    var src = document.getElementById("newsSource");
+    src.appendChild(img);
+
+    // clear the previous contents displayed
+    clear();
+
+    // get the result 
+    if (selection === "Gardian") {
+
       function buildQueryURL1() {
         // queryURL is the url we'll use to query the API
         var queryURL = "https://content.guardianapis.com/search?";
-      
+
         // Begin building an object to contain our API call's query parameters
         // Set the API key
-        var queryParams = { 
-            "api-key": "test" };
-      
+        var queryParams = {
+          "api-key": "test"
+        };
+
         // Grab text the user typed into the search input, add to the queryParams object
         queryParams.q = $("#search-term").val().trim();
-        console.log (queryParams.q);
-      
+        console.log(queryParams.q);
+
         // If the user provides a startYear, include it in the queryParams object
         var startYear = $("#start-year").val()
         // it does not work due to the free vesion of API
         if (parseInt(startYear)) {
-        // queryParams["from-date"] = startYear + "0101";
+          // queryParams["from-date"] = startYear + "0101";
         }
- 
-                
+
+
         return queryURL + $.param(queryParams);
       }
-      var queryURL=buildQueryURL1()
+      var queryURL = buildQueryURL1()
       console.log(queryURL)
-    
-    
-        // if the search value is not empty then pass the url to get the data
-        if(queryURL !=="") {
-    
-          $.ajax({
-    
-            url : queryURL,
-            method : "GET",
-            dataType :"json",
-    
-            beforsend: function (){
-              //any condition to be checked before sending
-            },
-            complete : function (){
-              // any condition to checked after this completed
-            },
-            
-            //when it is successfull 
-            success :function(news){
+
+
+      // if the search value is not empty then pass the url to get the data
+      if (queryURL !== "") {
+
+        $.ajax({
+
+          url: queryURL,
+          method: "GET",
+          dataType: "json",
+
+          beforsend: function () {
+            //any condition to be checked before sending
+          },
+          complete: function () {
+            // any condition to checked after this completed
+          },
+
+          //when it is successfull 
+          success: function (news) {
             //let the output to be empty at inital state
-            let output =""
+            let output = ""
             // let the result as latest news based on the response received
-            let latestNews =news.response.results;
+            let latestNews = news.response.results;
             // looping array for each object
-            for (var i in latestNews){
-            //append the output with the result
-              output +=`
+            for (var i in latestNews) {
+              //append the output with the result
+              output += `
 
 
               <div class="w3-card-4 " style="width:270%;">
@@ -99,81 +100,81 @@ $(document).ready (function(){
               
               
               `;
-    
-              }
-              //if the output is not empty
-              if (output !==""){
-              // append the  article section (id) in html with the result         
-                $("#article-section").append( output )
-             
-              // if the output is empty append this with '"There is no news"
-              } else {
-                $("#article-section").html("There is no news");
-    
-              }
-            },
-            // if any error found 
-            error:function() {
-              console.log("error");
+
             }
-    
-          });
-        } else{
-          // any condition??
-        }
-      
-      } else{
-        function buildQueryURL() {
-          // queryURL is the url we'll use to query the API
-          var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
-        
-          // Begin building an object to contain our API call's query parameters
-          // Set the API key
-          var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
-        
-          // Grab text the user typed into the search input, add to the queryParams object
-          queryParams.q = $("#search-term").val().trim();;
-          console.log (queryParams.q);
-        
-          // If the user provides a startYear, include it in the queryParams object
-          var startYear = $("#start-year").val()
-        
-          if (parseInt(startYear)) {
-            queryParams.begin_date  = startYear + "0101";
+            //if the output is not empty
+            if (output !== "") {
+              // append the  article section (id) in html with the result         
+              $("#article-section").append(output)
+
+              // if the output is empty append this with '"There is no news"
+            } else {
+              $("#article-section").html("There is no news");
+
+            }
+          },
+          // if any error found 
+          error: function () {
+            console.log("error");
           }
-                             
-          return queryURL + $.param(queryParams);
+
+        });
+      } else {
+        // any condition??
+      }
+
+    } else {
+      function buildQueryURL() {
+        // queryURL is the url we'll use to query the API
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+        // Begin building an object to contain our API call's query parameters
+        // Set the API key
+        var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+
+        // Grab text the user typed into the search input, add to the queryParams object
+        queryParams.q = $("#search-term").val().trim();;
+        console.log(queryParams.q);
+
+        // If the user provides a startYear, include it in the queryParams object
+        var startYear = $("#start-year").val()
+
+        if (parseInt(startYear)) {
+          queryParams.begin_date = startYear + "0101";
         }
-        var queryURL=buildQueryURL()
-        console.log(queryURL)
-      
-      
-          // if the search value is not empty then pass the url to get the data
-          if(queryURL !=="") {
-      
-            $.ajax({
-      
-              url : queryURL,
-              method : "GET",
-              dataType :"json",
-      
-              beforsend: function (){
-                //any condition to be checked before sending
-              },
-              complete : function (){
-                // any condition to checked after this completed
-              },
-              
-              //when it is successfull 
-              success :function(news){
-              //let the output to be empty at inital state
-              let output =""
-              // let the result as latest news based on the response received
-              let latestNews =news.response.docs;
-              // looping array for each object
-              for (var i in latestNews){
+
+        return queryURL + $.param(queryParams);
+      }
+      var queryURL = buildQueryURL()
+      console.log(queryURL)
+
+
+      // if the search value is not empty then pass the url to get the data
+      if (queryURL !== "") {
+
+        $.ajax({
+
+          url: queryURL,
+          method: "GET",
+          dataType: "json",
+
+          beforsend: function () {
+            //any condition to be checked before sending
+          },
+          complete: function () {
+            // any condition to checked after this completed
+          },
+
+          //when it is successfull 
+          success: function (news) {
+            //let the output to be empty at inital state
+            let output = ""
+            // let the result as latest news based on the response received
+            let latestNews = news.response.docs;
+            // looping array for each object
+            for (var i in latestNews) {
               //append the output with the result
-                output +=`
+              output += `
                 
 
                 <div class="w3-card-4" style="width:270%;">
@@ -195,39 +196,38 @@ $(document).ready (function(){
                 
                                                               
                 `;
-      
-                }
-                //if the output is not empty
-                if (output !==""){
-                // append the  article section (id) in html with the result         
-                  $("#article-section").append( output )
-               
-                // if the output is empty append this with '"There is no news"
-                } else {
-                  $("#article-section").html("There is no news");
-      
-                }
-              },
-              // if any error found 
-              error:function() {
-                console.log("error");
-              }
-      
-            });
-          } else{
-            // any condition??
+
+            }
+            //if the output is not empty
+            if (output !== "") {
+              // append the  article section (id) in html with the result         
+              $("#article-section").append(output)
+
+              // if the output is empty append this with '"There is no news"
+            } else {
+              $("#article-section").html("There is no news");
+
+            }
+          },
+          // if any error found 
+          error: function () {
+            console.log("error");
           }
+
+        });
+      } else {
+        // any condition??
       }
-  
-  
-    });
-    
-// Function to empty out the articles
-function clear() {
-  $("#article-section").empty();
-}
-   
-    });
-  
-  
-  
+    }
+
+
+  });
+
+  // Function to empty out the articles
+  function clear() {
+    $("#article-section").empty();
+  }
+
+});
+
+
